0298470dfec1fd7f891199da9a23eae53abc1251
unknown <henrimavi123@gmail.com>
2023-10-06 11:40:36 -0500

    Commit #2: Trabajar en el login y register en un solo componente

--- frontend-ips/src/app/app-routing.module.ts
+++ frontend-ips/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  // Otras rutas de la aplicación aquí
+  { path: 'login', component: LoginComponent }, // Ruta para la página de inicio de sesión
+  { path: '', redirectTo: '/login', pathMatch: 'full' }, // Redirige a la página de inicio de sesión por defecto
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- frontend-ips/src/app/app.module.ts
+++ frontend-ips/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
 import { HomeComponent } from './components/home/home/home.component';
 import { ControlRegisterComponent } from './components/home/control-register/control-register.component';
 import { ReportsComponent } from './components/home/reports/reports.component';
@@ -13,7 +12,6 @@ import { ReportsComponent } from './components/home/reports/reports.component';
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent,
     HomeComponent,
     ControlRegisterComponent,
     ReportsComponent
